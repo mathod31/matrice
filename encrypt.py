db4e0756ee1e40287e4984a86057c41b5b84824a
Mathieu Olives-Delmas <mathod@hotmail.fr>
2020-02-02 21:28:20 +0100

    finished

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,16 @@
-from _operator import is_not
-from functools import partial
-
 import numpy as np
+from random import randrange
 
 
 def matriceH(l: int):
     mat = [] * 7
+    cpt = 2 ** l - 1;
     for i in range(1, 2 ** l):
         mat.append(np.binary_repr(i, width=l))
         if is_power2(i):
             mat.insert(0, np.binary_repr(i, width=l))
             mat.pop()
+
 
     mat = list(zip(*mat))
     newMat = []
@@ -33,20 +33,99 @@ def matriceG(matH, l):
         newMat.append(m[newMatSize:])
 
     matTranspose = np.transpose(newMat).tolist()
-
     newIdentity = newIdentity.tolist()
-
-    print("new identity = ", newIdentity)
-    print("transpose = ", matTranspose)
-
     matG = []
     cpt = 0
     for m in newIdentity:
         matG.append(m + matTranspose[cpt])
         cpt += 1
 
-    print("mat G = ", matG)
+    return matG
 
-    # print(np.concatenate())
 
-    matTransmpose = np.transpose(newMat)
+def convertMotToMatrix(mot):
+    newMot = []
+    for i in mot:
+        newMot.append(i)
+
+    return newMot
+
+
+def convertMotEncodeInF2(mat):
+    mot = ''
+    for m in mat:
+        mot += str(m % 2)
+    return mot
+
+
+def convertMotDecodeInF2(mat):
+    mot = ''
+    for m in mat:
+        for i in m:
+            mot += str(i % 2)
+    return mot
+
+
+def encode(matG, mot):
+    mot = convertMotToMatrix(mot)
+
+    mot = np.array(mot, dtype='int')
+    matG = np.array(matG, dtype='int')
+    motEncode = mot @ matG
+    motEncode = convertMotEncodeInF2(motEncode)
+    return motEncode
+
+
+def modifyBitByPostion(mot, position):
+    mot = list(mot)
+    newValue = str((int(mot[position]) + 1) % 2)
+    mot[position] = newValue
+    return mot
+
+
+def bruitage(mot):
+    lenght = len(mot)
+    rand = randrange(lenght - 1)
+    mot = modifyBitByPostion(mot, rand)
+
+    return mot
+
+
+def getSyndromePosition(matH, motCorrection):
+    numberCol = len(matH[0])
+    motCorrection = np.array(list(motCorrection), dtype='int')
+
+    for c in range(0, numberCol):
+        if np.array_equal(matH[:, c], motCorrection):
+            return c
+    return None
+
+
+def correction(matH, mot):
+    motNotModify = mot
+    mot = [convertMotToMatrix(mot)]
+    motTranspose = np.transpose(mot)
+
+    mot = np.array(motTranspose, dtype='int')
+    matH = np.array(matH, dtype='int')
+
+    syndrome = matH @ mot
+
+    syndrome = convertMotDecodeInF2(syndrome)
+    syndromePosition = getSyndromePosition(matH, syndrome)
+    if syndromePosition is not None:
+        motCorrected = modifyBitByPostion(motNotModify, syndromePosition)
+    else:
+        motCorrected = 'Correct Word'
+
+    print(motNotModify, " --> ", ''.join(motCorrected))
+
+
+def hachage(matH, motEncode, l):
+    length = len(motEncode)
+    sizeCut = 2 ** l - 1
+    mots = [motEncode[y - sizeCut:y] for y in range(sizeCut, len(motEncode) + sizeCut, sizeCut)]
+    print('mots hashés : ', mots)
+    print('décodage :')
+    for mot in mots:
+        correction(matH, mot)
--- main.py
+++ main.py
@@ -4,8 +4,27 @@ import time
 if __name__ == '__main__':
     start_time = time.time()
     l = 3
+    motToDecode = '1011101100011000110011111010001110000111011011111'
     matriceH = encrypt.matriceH(l)
+    print('matriceH = ')
     print(matriceH)
-    matriceG = encrypt.matriceG(matriceH, l)
+    print('\n')
 
+    matriceG = encrypt.matriceG(matriceH, l)
+    print('matriceG = ')
+    print(matriceG)
+    print('\n')
+
+    toEncode = '1100'
+    encode = encrypt.encode(matriceG, toEncode)
+    print("mot :", toEncode, ", encodé --> ", encode)
+    print('\n')
+
+    toModify = '1100101'
+    bruitage = encrypt.bruitage(toModify)
+    print("mot : ", toModify, ", bruitage --> ", ''.join(bruitage))
+    print('\n')
+
+    # encrypt.correction(matriceH, '0011101')
+    encrypt.hachage(matriceH, '1011101100011000110011111010001110000111011011111', l)
     print("--- %s seconds ---" % (time.time() - start_time))
